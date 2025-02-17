30d0b03ed330dea3dc9270307833fcc11ed8eb7b
Miraç Kurtak <119788849+mirackurtak7@users.noreply.github.com>
2025-02-17 18:40:31 +0300

    Update mpu6050driver_launch.py

--- ros2_mpu6050_odom/launch/mpu6050driver_launch.py
+++ ros2_mpu6050_odom/launch/mpu6050driver_launch.py
@@ -3,22 +3,22 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-
 import os
-
 
 def generate_launch_description():
     ld = LaunchDescription()
+    
     share_dir = get_package_share_directory('mpu6050driver')
     parameter_file = LaunchConfiguration('params_file')
 
-    params_declare = DeclareLaunchArgument('params_file',
-                                           default_value=os.path.join(
-                                               share_dir, 'params', 'mpu6050.yaml'),
-                                           description='Path to the ROS2 parameters file to use.')
-
+    params_declare = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(share_dir, 'params', 'mpu6050.yaml'),
+        description='Path to the ROS2 parameters file to use.'
+    )
+    
     mpu6050driver_node = Node(
-        package='mpu6050driver',
+        package='mpu6050driver',  # Paket ismini kontrol edin (repo adı ros2_mpu6050_odometry ise hatalı)
         executable='mpu6050driver',
         name='mpu6050driver_node',
         output="screen",
@@ -26,6 +26,16 @@ def generate_launch_description():
         parameters=[parameter_file]
     )
 
+    odom_node = Node(  # Indentation hatası düzeltildi
+        package='mpu6050driver',
+        executable='odom',
+        name='odometry_node',
+        output='screen',
+        emulate_tty=True
+    )
+
     ld.add_action(params_declare)
     ld.add_action(mpu6050driver_node)
+    ld.add_action(odom_node)
+    
     return ld
